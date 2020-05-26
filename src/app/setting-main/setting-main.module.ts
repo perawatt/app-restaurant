e494606f5a9437ccbd6209bfd9437676360f0b40
Kritsana <tragoolphrom@gmail.com>
2020-05-26 17:49:29 +0700

    implement DateConverterPipe

--- src/app/setting-main/setting-main.module.ts
+++ src/app/setting-main/setting-main.module.ts
@@ -1,3 +1,4 @@
+import { PipesModule } from './../../pipes/pipes.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -13,8 +14,9 @@ import { SettingMainPage } from './setting-main.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    SettingMainPageRoutingModule
+    SettingMainPageRoutingModule,
+    PipesModule,
   ],
   declarations: [SettingMainPage]
 })
-export class SettingMainPageModule {}
+export class SettingMainPageModule { }
