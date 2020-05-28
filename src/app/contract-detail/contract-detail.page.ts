979e1cdbe618e0cdf01b813c27d8f155647633f6
woraput <woraput@kkumail.com>
2020-05-28 17:07:32 +0700

    new set title

--- src/app/contract-detail/contract-detail.page.ts
+++ src/app/contract-detail/contract-detail.page.ts
@@ -9,18 +9,20 @@ import { RestaurantService } from 'src/services/restaurant.service';
   styleUrls: ['./contract-detail.page.scss'],
 })
 export class ContractDetailPage implements OnInit {
-  
+
   public data$ = Promise.resolve([]);
   public deliveryName: string;
   public deliveryId: string;
   constructor(private route: ActivatedRoute, private nativeSvc: NativeService, private restaurantSvc: RestaurantService) {
     this.route.params.subscribe(param => { this.deliveryId = param["deliveryId"] });
-    this.route.params.subscribe(param => { this.deliveryName = param["deliveryName"] });
   }
 
   ngOnInit() {
-    this.nativeSvc.SetPageTitle(this.deliveryName);
     this.data$ = this.restaurantSvc.getDeliveryServiceById(this.deliveryId);
+    this.data$.then((it: any) => {
+      this.deliveryName = it.name;
+      this.nativeSvc.SetPageTitle(this.deliveryName);
+    });
   }
 
 }
