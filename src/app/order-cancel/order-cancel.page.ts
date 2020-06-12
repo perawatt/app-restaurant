5f7d2f47ec0f7fe2142f9e4e7fabf23cfa33d09d
powerofwind <powerofwind10@gmail.com>
2020-06-12 17:06:08 +0700

    add error message

--- src/app/order-cancel/order-cancel.page.ts
+++ src/app/order-cancel/order-cancel.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NativeService } from '../../providers/NativeService';
 import { ActivatedRoute } from '@angular/router';
 import { RestaurantService } from '../../services/restaurant.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-order-cancel',
@@ -12,23 +13,56 @@ export class OrderCancelPage implements OnInit {
 
   public orderId: string;
   public data$ = Promise.resolve([]);
-  constructor(private nativeSvc: NativeService, private route: ActivatedRoute, private restaurantSvc: RestaurantService) {
+  constructor(private nativeSvc: NativeService, private route: ActivatedRoute, private restaurantSvc: RestaurantService, private alertCtr: AlertController) {
     this.route.params.subscribe(param => { this.orderId = param["orderId"] });
   }
 
   ngOnInit() {
   }
 
-  ionViewDidEnter() {
+  ionViewWillEnter() {
     this.nativeSvc.SetPageTitle("ยกเลิกคำสั่งซื้อ");
+    this.getOrderInfo()
+  }
+
+  async getOrderInfo() {
+    const alert = await this.alertCtr.create({
+      header: 'เกิดข้อผิดพลาด',
+      message: "",
+      buttons: [{
+        text: 'ตกลง',
+        handler: () => {
+          this.nativeSvc.GoBack();
+        },
+      }],
+      backdropDismiss: false
+    });
     this.data$ = this.restaurantSvc.getOrderInfo(this.orderId);
+    this.data$.then((it: any) => {
+    }, async error => {
+      alert.message = error.error.message;
+      await alert.present();
+    });
   }
 
   async cancelOrderConfirm() {
-    await this.restaurantSvc.createOrderCancelRequest(this.orderId, { heading: "ไม่สามารถทำอาหารได้", info: "" }).then(() => 
-    { 
-      this.nativeSvc.GoBack();  
-    })
+    const alert = await this.alertCtr.create({
+      header: 'เกิดข้อผิดพลาด',
+      message: "",
+      buttons: [{
+        text: 'ตกลง',
+        handler: () => {
+          this.getOrderInfo();
+        },
+      }],
+      backdropDismiss: false
+    });
+    await this.restaurantSvc.createOrderCancelRequest(this.orderId, { heading: "ไม่สามารถทำอาหารได้", info: "" }).then(() => {
+      this.nativeSvc.GoBack();
+    }, async error => {
+      alert.message = error.error.message;
+      await alert.present();
+    });
   }
 
 }
